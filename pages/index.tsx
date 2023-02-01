614f56b4224fd3645da209675aef10465a282f81
cristianfrum <81685230+cristianfrum@users.noreply.github.com>
2023-02-01 14:38:18 +0000

    - Added comments in index.tsx - Created "utils" folder containing "TheVault.json" - contract ABI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import abi from '../utils/TheVault.json';
 
 declare global {
   interface Window {
@@ -7,19 +8,28 @@ declare global {
 }
 
 const IndexPage = () => {
+  // Contract Address & ABI
+  const contractAddress = "0x9162e4693Fdb524794a1251067fD1EA969102C47";
+  const contractABI = abi.abi;
+
+  // Initializing state variables
   const [ethAddress, setEthAddress] = React.useState(null)
   const [isLoading, setIsLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
 
+  //Check if MetaMask is installed
   const isMetaMaskAvailable = typeof window !== 'undefined' && window.ethereum
 
+  //
   React.useEffect(() => {
     if (!isMetaMaskAvailable) return
 
+    //Update the state whenever the address changes
     window.ethereum.on('accountsChanged', async (addresses: any[]) => {
       setEthAddress(addresses[0])
     });
 
+    //Update the state when the user logs in
     window.ethereum.request({ method: 'eth_requestAccounts' }).then((addresses: any) => {
       setEthAddress(addresses[0])
     })
