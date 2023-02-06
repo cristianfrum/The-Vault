33a2e85da97680456918a0a9c7b93ead68587cf2
cristianfrum <81685230+cristianfrum@users.noreply.github.com>
2023-02-06 16:18:49 +0000

    - Added the possibility to take as input the address, first and last names of the users, if any - Built dynamic list for users data - Enhanced the UI for testing

--- pages/CreateVault.js
+++ pages/CreateVault.js
@@ -5,12 +5,21 @@ import { ethers } from 'ethers';
 
 const CreateVault = () => {
   const [walletName, setWalletName] = React.useState('');
-  const contractAddress = "0xbDc6eB6465904Eba59C194Ab8124B1D03Fe20763";
+  const [walletBalance, setWalletBalance] = React.useState('');
+  const contractAddress = "0x3e85bb1e7fd4A8e808151836bA5303A82a26eceF";
   const contractABI = abi.abi;
+  const [membersAddresses, setMembersAddresses] = React.useState([]);
+  const [membersFirstNames, setMembersFirstNames] = React.useState([]);
+  const [membersLastNames, setMembersLastNames] = React.useState([]);
 
   const initializeWalletName = (event) => {
     setWalletName(event.target.value);
     console.log("input: " + walletName);
+  };
+
+  const initializeWalletBalance = (event) => {
+    setWalletBalance(event.target.value);
+    console.log("input: " + walletBalance);
   };
 
   const createTheWallet = async () => {
@@ -27,8 +36,9 @@ const CreateVault = () => {
         );
 
         console.log("creating the wallet..");
-        const vault = await theVault.initializeWallet(walletName, {
-          value: ethers.utils.parseEther('0.001'),
+
+        const vault = await theVault.initializeWallet(walletName, membersAddresses, membersFirstNames, membersLastNames, {
+          value: ethers.utils.parseEther(walletBalance),
           gasPrice: ethers.utils.parseUnits('20', 'gwei'),
         });
 
@@ -60,7 +70,7 @@ const CreateVault = () => {
         );
 
         console.log("fetching info from the blockchain..");
-        const info = await theVault.getWalletId("Wallet4");
+        const info = await theVault.getWalletId(walletName);
         console.log("fetched!");
         console.log(info);
         //  setMemos(memos);
@@ -73,15 +83,99 @@ const CreateVault = () => {
     }
   };
 
+  const getWalletBalance = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const theVault = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+        console.log("fetching info from the blockchain..");
+        const info = await theVault.getWalletBalance(walletName) / 10 ** 18;
+        console.log("fetched!");
+        console.log(info);
+      } else {
+        console.log("Metamask is not connected");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleAddInput = () => {
+    setMembersAddresses([...membersAddresses, '']);
+    setMembersFirstNames([...membersFirstNames, '']);
+    setMembersLastNames([...membersLastNames, '']);
+  };
+
+  const handleChangeAddresses = (e, index) => {
+    const values = [...membersAddresses];
+    values[index] = e.target.value;
+    setMembersAddresses(values);
+  };
+
+  const handleChangeFirstNames = (e, index) => {
+    const values = [...membersFirstNames];
+    values[index] = e.target.value;
+    setMembersFirstNames(values);
+  };
+
+  const handleChangeLastNames = (e, index) => {
+    const values = [...membersLastNames];
+    values[index] = e.target.value;
+    setMembersLastNames(values);
+  };
+
   return (
     <div>
       <h1>Welcome to the Vault</h1>
       <input type="text" placeholder="Type wallet's name..." value={walletName} onChange={initializeWalletName} />
-      <button onClick={createTheWallet}>Create the wallet</button>
-      <button onClick={getWalletId}>Get wallet's id</button>
-      <Link href="/">
-        <button>Go to the home page</button>
-      </Link>
+      <button onClick={handleAddInput}>Add New Member</button>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {membersAddresses.map((input, index) => (
+            <input type="text" placeholder="Type user' address..."
+              value={input.value}
+              onChange={e => handleChangeAddresses(e, index)}
+            />
+          ))}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {membersFirstNames.map((input, index) => (
+            <input type="text" placeholder="Type user' first name..."
+              value={input.value}
+              onChange={e => handleChangeFirstNames(e, index)}
+            />
+          ))}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          {membersLastNames.map((input, index) => (
+            <input type="text" placeholder="Type users' last name..."
+              value={input.value}
+              onChange={e => handleChangeLastNames(e, index)}
+            />
+          ))}
+        </div>
+      </div>
+      <input type="text" placeholder="Set wallet's balance..." value={walletBalance} onChange={initializeWalletBalance} />
+      <div>
+        <button onClick={createTheWallet}>Create the wallet</button>
+      </div>
+      <div>
+        <button onClick={getWalletId}>Get wallet's id</button>
+      </div>
+      <div>
+        <button onClick={getWalletBalance}>Get wallet's balance</button>
+      </div>
+      <div>
+        <Link href="/">
+          <button>Go to the home page</button>
+        </Link>
+      </div>
     </div>)
 
 }
